7685d47eba1e1ad6e3ca46f2831b4561bcaa7296
maxm2084 <96729107+maxm2084@users.noreply.github.com>
2022-11-10 08:18:37 -0300

    Add files via upload

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,11 +1,1 @@
-# Hola Mundo! [Python]
-# ---------------------------
-# Autor: Inove Coding School
-# Version: 2.0
-
-# Descripcion:
-# Programa creado para ensayar el correcto funcionamiento
-# del entorno de instalación Python
-
-
-print("Hola Mundo!")
+print("Hola Mundo.")
